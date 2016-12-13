3a15e95e45bb91d82b975f83a29b8819d378c1a6
dangerstephen <dangersteve1@gmail.com>
2016-12-13 15:59:48 -0800

    can now use the form to add new songs. will now make update and delete options

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,8 +11,25 @@ class RecordsController < ApplicationController
   end
 
   def new
-    @record = Record.new
+    @record = Record.new()
     render :new
   end
+
+  def create
+  Record.create(record_params)
+  redirect_to('/records')
+end
+
+def update
+Record.update(params[:id], record_params)
+end
+
+
+private # Everything below is only able to be used in this controller
+
+def record_params
+  params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+
+end
 
 end
