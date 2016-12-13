e126d774b5167df8fdae9584cff4ac66c20d099f
dangerstephen <dangersteve1@gmail.com>
2016-12-13 15:42:48 -0800

    added a route to show record by id and a basic form to add a new record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,18 @@
 class RecordsController < ApplicationController
   def index
   @records = Record.all
+  render :index
   end
+
+  def show
+    record_id = params[:id]
+    @record = Record.find(record_id)
+    render :show
+  end
+
+  def new
+    @record = Record.new
+    render :new
+  end
+
 end
